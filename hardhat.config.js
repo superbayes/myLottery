f39478c030d270053a93bde026eba30f11a28447
superbayes <nanyangjx@126.com>
2024-08-18 07:04:46 +0800

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,12 +21,17 @@ module.exports = {
     sepolia: {
       url: "https://sepolia.infura.io/v3/" + process.env.INFURA_ID, // Sepolia网络的RPC URL
       chainId: 11155111, // Sepolia网络的Chain ID
-      accounts: [process.env.PRIVATE_KEY] // 使用你的私钥
+      accounts: [process.env.PRIVATE_KEY] // 私钥
     },
     bsctestnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545/", // BSC测试网的RPC URL
       chainId: 97, // BSC测试网的Chain ID
       accounts: [process.env.PRIVATE_KEY] // 私钥
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56, // BSC主网的链ID
+      accounts: [process.env.PRIVATE_KEY] //账户私钥（注意保密）
     }
   }
 };
