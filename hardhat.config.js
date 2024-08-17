935c2f771df6b19c77957225a42f0bfe49fa97ac
superbayes <nanyangjx@126.com>
2024-08-17 21:19:22 +0800

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('@openzeppelin/hardhat-upgrades');
+require("dotenv").config();
+
+//require("@nomiclabs/hardhat-ganache");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.20",
+  defaultNetwork: "localhost",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.g.alchemy.com/v2/" + process.env.AICHEMY_ID,
+        //blockNumber: 20291600 // 可选
+      }
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+   },
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/" + process.env.INFURA_ID, // Sepolia网络的RPC URL
+      chainId: 11155111, // Sepolia网络的Chain ID
+      accounts: [process.env.PRIVATE_KEY] // 使用你的私钥
+    },
+    bsctestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/", // BSC测试网的RPC URL
+      chainId: 97, // BSC测试网的Chain ID
+      accounts: [process.env.PRIVATE_KEY] // 私钥
+    }
+  }
 };
